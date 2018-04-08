837e476329b41fdf61bc1c0465187f986e39c35d
Josh Bay <joshbay@Joshs-MacBook-Pro.local>
2018-04-08 12:15:55 -0500

    updated packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => (
