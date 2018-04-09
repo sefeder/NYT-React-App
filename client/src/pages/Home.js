b756893ace8c6e4afb035390335975fd7c7f1583
Josh Bay <joshbay@Joshs-MacBook-Pro.local>
2018-04-09 11:21:16 -0500

    made article links open in new tab

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,96 +3,124 @@ import API from "../utils/API";
 import axios from "axios";
 
 class Home extends Component {
-    constructor() {
-        super();
-        this.state = {
-            articles: [],
-        }
-    }
-    submitSearch = event => {
-        event.preventDefault();
-        let topic = event.target.children[0].children[1].value
-        let startYear = event.target.children[1].children[0].children[1].value
-        let endYear = event.target.children[1].children[1].children[1].value
-        API.getArticles(topic, startYear, endYear)
-        .then(res => {
-           this.setState({ articles: res.data.response.docs})
-        })
-    }
-    saveArticle = event => {
-        event.preventDefault();
-        event.target.setAttribute('disabled', true);
-        event.target.classList.add('btn-default');
-        event.target.classList.remove('btn-success');
-        event.target.textContent = 'Saved';
-        let headline = event.target.previousSibling.previousSibling.previousSibling.textContent
-        let date = event.target.previousSibling.previousSibling.textContent
-        let URL = event.target.previousSibling.textContent
-        axios.post('http://localhost:4025/api/articles', {
-            headline: headline,
-            date: date,
-            URL: URL
-        })
-    }
+  constructor() {
+    super();
+    this.state = {
+      articles: []
+    };
+  }
+  submitSearch = event => {
+    event.preventDefault();
+    let topic = event.target.children[0].children[1].value;
+    let startYear = event.target.children[1].children[0].children[1].value;
+    let endYear = event.target.children[1].children[1].children[1].value;
+    API.getArticles(topic, startYear, endYear).then(res => {
+      this.setState({ articles: res.data.response.docs });
+    });
+  };
+  saveArticle = event => {
+    event.preventDefault();
+    event.target.setAttribute("disabled", true);
+    event.target.classList.add("btn-default");
+    event.target.classList.remove("btn-success");
+    event.target.textContent = "Saved";
+    let headline =
+      event.target.previousSibling.previousSibling.previousSibling.textContent;
+    let date = event.target.previousSibling.previousSibling.textContent;
+    let URL = event.target.previousSibling.textContent;
+    axios.post("http://localhost:4025/api/articles", {
+      headline: headline,
+      date: date,
+      URL: URL
+    });
+  };
 
-    render() {
-        return (
-            <div className="App">
-                <h1>
-                    NYT Search App
-                </h1>
-                <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h2>Search</h2>
-                    </div>
-                    <div className="panel-body container">
-                        <form id="form" onSubmit={this.submitSearch} >
-                            <div className="form-group">
-                                <label htmlFor="topicSearch">Topic:</label>
-                                <input className="form-control" id="topicSearch" type="text" placeholder="eg. Barack Obama" />
-                            </div>
-                            <div className="row">
-                                <div className="form-group col-xs-6">
-                                    <label htmlFor="SYSearch">Start Year:</label>
-                                    <input className="form-control" id="SYSearch" type="text" placeholder="eg. 1975" />
-                                </div>
-                                <div className="form-group col-xs-6">
-                                    <label htmlFor="EYSearch">End Year:</label>
-                                    <input className="form-control" id="EYSearch" type="text" placeholder="eg. 2018" />
-                                </div>
-                            </div>
-                            <div className="row">
-                                <div className="form-group">
-                                    <input className="btn btn-success col-xs-offset-2 col-xs-8" type="submit" value="Search" />
-                                </div>
-                            </div>
-                        </form>
-                    </div>
+  render() {
+    return (
+      <div className="App">
+        <h1>NYT Search App</h1>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h2>Search</h2>
+          </div>
+          <div className="panel-body container">
+            <form id="form" onSubmit={this.submitSearch}>
+              <div className="form-group">
+                <label htmlFor="topicSearch">Topic:</label>
+                <input
+                  className="form-control"
+                  id="topicSearch"
+                  type="text"
+                  placeholder="eg. Barack Obama"
+                />
+              </div>
+              <div className="row">
+                <div className="form-group col-xs-6">
+                  <label htmlFor="SYSearch">Start Year:</label>
+                  <input
+                    className="form-control"
+                    id="SYSearch"
+                    type="text"
+                    placeholder="eg. 1975"
+                  />
                 </div>
-                <div className="panel panel-default">
-                    <div className="panel-heading">
-                        <h2>Results</h2>
-                    </div>
-                    <div className="panel-body">
-        {this.state.articles.length > 0 ? this.state.articles.map(a => (
-                            <div className="panel panel-default" key={a._id}>
-                                <a target="_blank" href={a.web_url}><h4>{a.headline.main}</h4></a>
-                                <h5>{a.pub_date}</h5>
-                                <h5>{a.web_url}</h5>
-                                <button className="btn btn-success" onClick={this.saveArticle}>Save</button>
-                                <br/><br/>
-                            </div>
-        )) : <h2>No results to display</h2>}
-                    
-                    </div>
-
+                <div className="form-group col-xs-6">
+                  <label htmlFor="EYSearch">End Year:</label>
+                  <input
+                    className="form-control"
+                    id="EYSearch"
+                    type="text"
+                    placeholder="eg. 2018"
+                  />
                 </div>
-            </div>
-        )
-    }
+              </div>
+              <div className="row">
+                <div className="form-group">
+                  <input
+                    className="btn btn-success col-xs-offset-2 col-xs-8"
+                    type="submit"
+                    value="Search"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h2>Results</h2>
+          </div>
+          <div className="panel-body">
+            {this.state.articles.length > 0 ? (
+              this.state.articles.map(a => (
+                <div className="panel panel-default" key={a._id}>
+                  <a target="_blank" href={a.web_url}>
+                    <h4>{a.headline.main}</h4>
+                  </a>
+                  <h5>{a.pub_date}</h5>
+                  <h5>
+                    <a href={a.web_url} target="_blank">
+                      {a.web_url}
+                    </a>
+                  </h5>
+                  <button
+                    className="btn btn-success"
+                    onClick={this.saveArticle}
+                  >
+                    Save
+                  </button>
+                  <br />
+                  <br />
+                </div>
+              ))
+            ) : (
+              <h2>No results to display</h2>
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-
-
 
 export default Home;
--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,51 +1,55 @@
 import React, { Component } from "react";
 
 class Saved extends Component {
-    constructor() {
-        super();
-        this.state = {
-            articles: [],
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      articles: []
+    };
+  }
 
-    componentDidMount() {
-        this.loadArticles()
-            .then(articles => this.setState({ articles }))
-    }
+  componentDidMount() {
+    this.loadArticles().then(articles => this.setState({ articles }));
+  }
 
-    loadArticles = () => {
-       return fetch('http://localhost:4025/api/articles').then(res => res.json())   
-    }
+  loadArticles = () => {
+    return fetch("http://localhost:4025/api/articles").then(res => res.json());
+  };
 
-    removeArticle = event => {
-        event.preventDefault();
-        let deleteID = event.target.parentElement.getAttribute('data-id');
-        fetch(`http://localhost:4025/api/articles/${deleteID}`, {
-            method: "DELETE"
-        })
-        .then(res => res.json())
-        .then(oldArticleID => {
-            let articles = this.state.articles.filter(
-                (article, i) => article._id !== oldArticleID
-            );
-            this.setState({articles})
-        });
-        
-    }
+  removeArticle = event => {
+    event.preventDefault();
+    let deleteID = event.target.parentElement.getAttribute("data-id");
+    fetch(`http://localhost:4025/api/articles/${deleteID}`, {
+      method: "DELETE"
+    })
+      .then(res => res.json())
+      .then(oldArticleID => {
+        let articles = this.state.articles.filter(
+          (article, i) => article._id !== oldArticleID
+        );
+        this.setState({ articles });
+      });
+  };
 
-    render() {
-        return (
-            <div className="panel-body">
-                {this.state.articles.map(a => (
-                    <div className="panel panel-default" data-id={a._id} key={a._id}>
-                        <h5>{a.title}</h5>
-                        <h5>{a.date}</h5>
-                        <h5>{a.URL}</h5>
-                        <button className="btn btn-danger" onClick={this.removeArticle}>Remove</button>
-                    </div>
-                ))}
-            </div>
-
-        )}
+  render() {
+    return (
+      <div className="panel-body">
+        {this.state.articles.map(a => (
+          <div className="panel panel-default" data-id={a._id} key={a._id}>
+            <h5>{a.title}</h5>
+            <h5>{a.date}</h5>
+            <h5>
+              <a href={a.URL} target="_blank">
+                {a.URL}
+              </a>
+            </h5>
+            <button className="btn btn-danger" onClick={this.removeArticle}>
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 export default Saved;
